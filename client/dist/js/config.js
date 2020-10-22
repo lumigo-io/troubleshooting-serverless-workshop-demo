429524e1aed949718663ef93a66eea4b927fb12e
theburningmonk <theburningmonk@gmail.com>
2020-10-22 19:31:19 +0200

    fix: added missing files

---
+++ client/dist/js/config.js
@@ -0,0 +1,11 @@
+window._config = {
+  "api": {
+    "invokeUrl": "https://bzx29xfarg.execute-api.us-east-1.amazonaws.com/dev/ride"
+  },
+  "cognito": {
+    "userPoolId": "us-east-1_EvAcmvQGw",
+    "userPoolClientId": "698ijjrmb0oneo7d4kgndo3a3o",
+    "region": "us-east-1",
+    "disabled": false
+  }
+}
